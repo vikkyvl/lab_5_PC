90e80eb347dce4aa3ae114aa1f39b4aab6d60935
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2025-04-18 22:45:10 +0300

    Added basic TCP connection handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <thread>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 8080
+
+int main()
+{
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        std::cerr << "WSAStartup failed.\n";
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Error creating socket.\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        std::cerr << "Binding error.\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
+    {
+        std::cerr << "Listening error.\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server started on port " << PORT << "." << std::endl;
+
+    closesocket(serverSocket);
+    WSACleanup();
     return 0;
 }
